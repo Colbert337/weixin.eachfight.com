97aac48a262dee7a04a086089bebd8c5a0fdca27
guochao <347653397@qq.com>
2017-10-23 22:44:02 +0800

    微信授权登陆修改

--- application/controllers/api/Comm.php
+++ application/controllers/api/Comm.php
@@ -3,11 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Comm extends CI_Controller
 {
-    private $wechat = 'wechat_user';
 
     public function index(){
-        $user = $this->session->userdata($this->wechat);
-        dump(100);
+
     }
+
 
 }
