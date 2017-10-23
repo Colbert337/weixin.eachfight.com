27146b5797b47635aa1448f198c0d9a6861e7636
guochao <347653397@qq.com>
2017-10-23 23:00:29 +0800

    修改

--- application/controllers/api/Weixin.php
+++ application/controllers/api/Weixin.php
@@ -33,7 +33,7 @@ class Weixin extends CI_Controller
     //回调地址，获取用户基本信息  第一次注册入库
     public function oauthBack(){
         $user = $this->wechat->oauth->user();
-        $userArr = $user->toArray();
+//        $userArr = $user->toArray();
         $this->session->set_userdata($this->wechat, $user->id);
         $user = $this->session->userdata($this->wechat);
         set_cookie('token',$user->id,time()+7200,'.eachfight.com','/');
