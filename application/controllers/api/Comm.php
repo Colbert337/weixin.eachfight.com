36c2921825677a2df8da37a5da2de9e50c1fbc67
guochao <347653397@qq.com>
2017-10-23 22:26:27 +0800

    修改

--- application/controllers/api/Comm.php
+++ application/controllers/api/Comm.php
@@ -1,14 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Common extends CI_Controller
+class Comm extends CI_Controller
 {
     private $wechat = 'wechat_user';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
     public function index(){
         $user = $this->session->userdata($this->wechat);
