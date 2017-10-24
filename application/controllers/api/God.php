ed39ab23d69e5efcad5dbccd7521e385fefd3ec9
fengchen <793788050@qq.com>
2017-10-24 11:35:59 +0800

    接入 REST API

--- application/controllers/api/God.php
+++ application/controllers/api/God.php
@@ -1,15 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class God extends CI_Controller
+class God extends MY_Controller
 {
 
     private $wechat = 'wechat_user';
 
-    public function index()
+    function __construct()
     {
-        dump($this->session->userdata());
-        $user = $this->session->userdata($this->wechat);
-        dump($user);
+        // Construct the parent class
+        parent::__construct();
+    }
+
+    public function index_get()
+    {
+
+        $data = ['pv'=>"1000", 'uv'=>"1000", 'date'=>time()];
+        $this->response($data);
     }
 }
