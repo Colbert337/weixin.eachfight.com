093e9670366f65dbfa655fb5df092a75ba7c9c52
guochao <guoch@yoozoo.com>
2017-10-25 10:41:03 +0800

    短信接入

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -6,20 +6,15 @@ class User extends CI_Controller
     public function index()
     {
         header('Content-Type: text/plain; charset=utf-8');
-        $demo = new SmsDemo(
-          
-        );
-
-        echo "SmsDemo::sendSms\n";
+        $demo = new \App\Libraries\Sms();
         $response = $demo->sendSms(
             "短信签名", // 短信签名
             "SMS_0000001", // 短信模板编号
-            "12345678901", // 短信接收者
+            "13127529625", // 短信接收者
             Array(  // 短信模板中字段的值
-                "code"=>"12345",
-                "product"=>"dsd"
+                "code"=>"123456",
             ),
-            "123"
+            "123456789"
         );
         print_r($response);
     }
