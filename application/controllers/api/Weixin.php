4174ffd99d37c28b2dfb1e4a8f5c9d0dc84fb316
guochao <347653397@qq.com>
2017-10-27 23:11:29 +0800

    微信授权

--- application/controllers/api/Weixin.php
+++ application/controllers/api/Weixin.php
@@ -1,8 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 use EasyWeChat\Foundation\Application;
 
-class Weixin extends MY_Controller
+class Weixin extends CI_Controller
 {
     private $wechat = 'wechat_user';
 
@@ -15,36 +16,41 @@ class Weixin extends MY_Controller
     }
 
     //根据用户openid获取用户基本信息
-    public function user(){
+    public function user()
+    {
         $user = $this->wechat->user->get("o05NB0w96SrxDgpS6ZzOapUNq1WY");
         var_dump($user);
     }
 
     //微信用户进行公众号授权
-    public function oauth(){
-        $callback = urldecode($this->input->get('url')).'?code=200';
-        if(!$this->session->has_userdata($this->wechat)){
-            $response = $this->wechat->oauth->with(['state'=>urlencode($callback)])->redirect();
+    public function oauth()
+    {
+        $callback = urldecode($this->input->get('url')) . '?code=200';
+        if (!$this->session->has_userdata($this->wechat)) {
+            $response = $this->wechat->oauth->with(['state' => urlencode($callback)])->redirect();
             $response->send();
-        }else{
+        } else {
             redirect($callback);
         }
     }
 
     //回调地址，获取用户基本信息  第一次注册入库
-    public function oauthBack(){
+    public function oauthBack()
+    {
 //      dump($this->wechat->oauth);exit;
         $user = $this->wechat->oauth->user();
         $userArr = $user->toArray();
-        $this->session->set_userdata(['wechat_user'=>$userArr['id']]);
-        set_cookie('token',$userArr['id'],time()+7200,'.eachfight.com','/');
+        $this->session->set_userdata(['wechat_user' => $userArr['id']]);
+        set_cookie('token', $userArr['id'], time() + 7200, '.eachfight.com', '/');
         redirect(urldecode($this->input->get('state')));
     }
 
     //前端给code 授权获取用户信息 注册入库
-    public function weboauth(){
+    public function weboauth()
+    {
         $user = $this->wechat->oauth->user();
-        set_cookie('token',$user->id,time()+7200,'.eachfight.com','/');
-        $this->responseJson(200, '数据获取成功', $user->toArray());
+        set_cookie('token', $user->id, time() + 7200, '.eachfight.com', '/');
+        echo json_encode(['code' => 200, 'msg' => '数据获取成功', 'data' => $user->toArray()]);
+        exit;
     }
 }
