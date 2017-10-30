4e032ee0d6ed6b3131b78220eff201cd9ebd6993
guochao <guoch@yoozoo.com>
2017-10-30 19:50:41 +0800

    修改

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,113 +1,56 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Home extends CI_Controller
 {
-    private $wechat = 'wechat_user';
 
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper('cookie');
     }
 
-	public function index()
-	{
-        dump($this->session->userdata($this->wechat));exit;
-
-        set_cookie('token',1000,time()+7200,'.eachfight.com','/');
-        dump(get_cookie('token'));
-        $zip = new \ZipArchive;
-        $toDir = "public/zip";
-        if (!file_exists($toDir)) {
-            mkdir($toDir);
-        }
-        if ($zip->open('test.zip') === TRUE) {
-            $zip->extractTo($toDir);//假设解压缩到在当前路径下images文件夹的子文件夹php
-            $zip->close();//关闭处理的zip文件
-        }
-        var_dump($zip);
-        exit;
-        exit;
-
-        $zip = new \ZipArchive;
-        $zipfile = "test.tar";
-        $res = $zip->open($zipfile);
-
-        $toDir = "./public/zip";
-        if (!file_exists($toDir)) {
-            mkdir($toDir);
-        }
-        $docnum = $zip->numFiles;
-        for ($i = 0; $i < $docnum; $i++) {
-            $statInfo = $zip->statIndex($i);
-            var_dump($statInfo);
-            exit;
-            if ($statInfo['crc'] == 0) {
-                //新建目录
-                mkdir($toDir . '/' . substr($statInfo['name'], 0, -1));
-            } else {
-                //拷贝文件
-                copy('zip://' . $zipfile . '#' . $statInfo['name'], $toDir . '/' . $statInfo['name']);
-            }
-        }
-
-        print_r(scandir($toDir));
-    }
-
-    public function test()
+    public function index()
     {
-//        var_dump(100);exit;
-//        $val = '';
-//        if ($val > 0 && $val < 2000) {
-//            return [1 => 0.1];
-//        } elseif ($val < 20000) {
-//            return [2 => 0.15];
-//        } elseif ($val < 200000) {
-//            return [3 => 0.2];
-//        } else {
-//            return [4 => 0.25];
-//        }
-
-        $arrayName = array(
-            '1' => array(
-                'one' => 0,
-                'two' => 2000,
-                'bai' => 0.1,
-            ),
-            '2' => array(
-                'one' => 2000,
-                'two' => 20000,
-                'bai' => 0.15,
-            ),
-            '3' => array(
-                'one' => 20000,
-                'two' => 200000,
-                'bai' => 0.2,
-            ),
-            '4' => array(
-                'one' => 200000,
-                'two' => 10000000000,
-                'bai' => 0.25,
-            ),
+        $proArr = array(
+            array('id' => 1, 'name' => '特等奖', 'v' => 1),
+            array('id' => 2, 'name' => '一等奖', 'v' => 5),
+            array('id' => 3, 'name' => '二等奖', 'v' => 10),
+            array('id' => 4, 'name' => '三等奖', 'v' => 12),
+            array('id' => 5, 'name' => '四等奖', 'v' => 22),
+            array('id' => 6, 'name' => '没中奖', 'v' => 50)
         );
 
-        $total = 500000; //$tp=4
-        $tp = 1;
-        foreach($arrayName as $key=>$val){
-           if($total>=$val['one'] && $total<$val['two']){
-               $tp = $key;
-           }
+        for ($i = 1; $i <=100; $i++) {
+            $arr[] = $this->get_rand($proArr);
         }
-
-        $money = 0;
-        for($i=$tp;$i>=1;$i--){
-            if($i==$tp){
-                $num = $total-$arrayName[$i]['one'];
-            }else{
-                $num = $arrayName[$i]['two']-$arrayName[$i]['one'];
+        foreach ($arr as $val){
+            if($val['id'] == 6 ){
+                $arr_new[]  = $val;
             }
-            $money += $num*$arrayName[$i]['bai'];
         }
-        var_dump($money);
+        dump(count($arr_new));
+    }
+
+
+    private function get_rand($proArr)
+    {
+        $result = array();
+        foreach ($proArr as $key => $val) {
+            $arr[$key] = $val['v'];
+        }
+        // 概率数组的总概率
+        $proSum = array_sum($arr);
+        asort($arr);
+        // 概率数组循环
+        foreach ($arr as $k => $v) {
+            $randNum = mt_rand(1, $proSum);
+            if ($randNum <= $v) {
+                $result = $proArr[$k];
+                break;
+            } else {
+                $proSum -= $v;
+            }
+        }
+        return $result;
     }
 }
