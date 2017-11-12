a476ee8a8b61c90c33c5a9fae876a71c1efe1ca7
liuwb <lwb@wr-soft.com>
2017-11-12 17:28:54 +0800

    后台框架整合

---
+++ application/models/Order_Model.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * GodApply Model Class
+ *
+ * 认证大神操作Model
+ * @category    Models
+ * @author      fengchen <fengchenorz@gmail.com>
+ */
+class Order_Model extends MY_Model {
+
+    const TBL = 'order';
+    
+    /**
+     * 主键：{"id"}
+     * 
+     * @access private
+     */
+    private $_unique_key = array('id');
+
+    /**
+     * 构造函数
+     *
+     * @access public
+     * @return void
+     */
+    function __construct()
+    {
+        parent::__construct();
+
+    }
+
+
+
+}
