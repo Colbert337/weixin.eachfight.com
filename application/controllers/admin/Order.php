fc7a46fbb6adb882f134d3de8652a272e9e28cfd
liuwb <lwb@wr-soft.com>
2017-12-10 21:27:36 +0800

    后台功能持续完善中。。。。

--- application/controllers/admin/Order.php
+++ application/controllers/admin/Order.php
@@ -9,28 +9,31 @@ class Order extends CI_Controller {
     {
         parent::__construct();
         $this->load->library('pagination');
+        $this->load->helper('used_helper');
         $this->load->model('Order_Model');
         $this->load->model('User_Model');
     }
 
 	public function index()
 	{
-		echo "111";exit;
-		// $where=[];
-		// $order=$this->Order_Model->fetchAll($where);
-		// foreach ($order as $key => $value) {
-		// 	//查出订单用户
-		// 	$userid=$value['user_id'];
-		// 	$user=$this->User_Model->scalar($userid);
-		// 	$order[$key]['username']=$user['nickname'];
+		$where=[];
+		$order=$this->Order_Model->fetchAll($where);
+		foreach ($order as $key => $value) {
+			//查出订单用户
+			$userid=$value['user_id'];
+			$user=$this->User_Model->scalar($userid);
+			$nickname=isset($user['nickname'])?$user['nickname']:"";
+			// dump($nickname);exit;
+			$order[$key]['username']=emoji_to_string($nickname);
 
-		// 	//查出大神用户
-		// 	$goduserid=$value['user_id'];
-		// 	$goduser=$this->User_Model->scalar($goduserid);
-		// 	$order[$key]['godusername']=$user['nickname'];
-		// }
-		// $data['count'] = count($order);
-		// $data['order']=$order;
-		// $this->load->view('admin/order/index',$data);
+			//查出大神用户
+			$goduserid=$value['user_id'];
+			$goduser=$this->User_Model->scalar($goduserid);
+			$order[$key]['godusername']=isset($goduser['nickname'])?emoji_to_string($goduser['nickname']):"";
+		}
+		// dump($order);exit;
+		$data['count'] = count($order);
+		$data['list']=$order;
+		$this->load->view('admin/order/index',$data);
 	}
 }
--- application/views/admin/template/menu.php
+++ application/views/admin/template/menu.php
@@ -1,5 +1,14 @@
 <aside class="Hui-aside">
     <div class="menu_dropdown bk_2">
+
+        <dl id="menu-member">
+            <dt><i class="Hui-iconfont">&#xe60d;</i> 大神申请<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+            <dd>
+                <ul>
+                    <li><a data-href="admin/GodApply/index" data-title="申请列表" href="javascript:;">申请列表</a></li>
+                </ul>
+            </dd>
+        </dl>
         <dl id="menu-comments">
             <dt><i class="Hui-iconfont">&#xe687;</i> 订单管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
             <dd>
@@ -9,10 +18,10 @@
             </dd>
         </dl>
         <dl id="menu-member">
-            <dt><i class="Hui-iconfont">&#xe60d;</i> 用户管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+            <dt><i class="Hui-iconfont">&#xe60d;</i>  订单申诉<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
             <dd>
                 <ul>
-                    <li><a data-href="admin/User/index" data-title="用户列表" href="javascript:;">用户列表</a></li>
+                    <li><a data-href="admin/User/index" data-title="订单申诉" href="javascript:;">订单申诉</a></li>
                 </ul>
             </dd>
         </dl>
