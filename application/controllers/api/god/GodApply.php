76a6e64135705fc01b6101472c963a7cc235c75d
guochao <347653397@qq.com>
2018-01-21 20:39:46 +0800

    大神注册信息提交接口  修改

--- application/controllers/api/god/GodApply.php
+++ application/controllers/api/god/GodApply.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * 大神申请接口
  * Class GodBattleRecord
- * @author	fengchen <fengchenorz@gmail.com>
+ * @author    fengchen <fengchenorz@gmail.com>
  * @time    21017/10/25
  */
 class GodApply extends MY_Controller
@@ -14,6 +14,9 @@ class GodApply extends MY_Controller
         // Construct the parent class
         parent::__construct();
         $this->load->model('GodApply_Model', 'godApply');
+
+        //获取用户uid
+        $this->user_id = $this->getUserId();
     }
 
     /**
@@ -22,17 +25,17 @@ class GodApply extends MY_Controller
     public function index_get()
     {
         $id = $this->uri->segment(3);
-        if(!empty($id)){
-            if (!is_numeric($id)){
+        if (!empty($id)) {
+            if (!is_numeric($id)) {
                 $this->responseJson(502, 'ID参数错误');
             }
             $data = $this->godApply->scalar($id);
-        }else{
+        } else {
             $data = $this->godApply->fetchAll();
         }
-        if(!empty($data)){
+        if (!empty($data)) {
             $this->responseJson(200, '数据获取成功', $data);
-        }else{
+        } else {
             $this->responseJson(502, '数据获取失败');
         }
 
@@ -41,18 +44,34 @@ class GodApply extends MY_Controller
     /**
      * 数据提交
      */
-    public function index_post(){
-
-        $flag = false;
+    public function index_post()
+    {
         $post = $this->input->post();
         $this->validPost($post);
+        //验证码校验
+        $mobile = $post['mobile'];
+        $key = "LAST_SMSCODE_{$mobile}";
+        $redis_code = $this->cache->redis->get($key);
+        if (!$redis_code) $this->responseToJson(502, '验证码已过期');
+        if ($redis_code != $post['code']) $this->responseToJson(502, '验证码错误');
+        //验证
+        $user_data = $this->User_Model->getUserById($this->user_id);
+        if ($user_data['is_god'] == 2)
+            $this->responseToJson(502, '你已经是大神身份');
         //获取当前登陆人信息
-        $post['user_id'] = "1";
-        $post['create_time'] = date("Y-m-d H:i:s",time());
-        // 提交数据
-        $data = $this->godApply->submitGodApply($post);
-        if ($data) $flag = true;
-        $flag ? $this->responseJson(200, '数据写入成功', $data) : $this->responseJson(200, '数据写入失败');
+        $post['user_id'] = $this->user_id;
+        $post['create_time'] = date("Y-m-d H:i:s", time());
+        //更新用户表
+        $result_1 = $this->User_Model->update(['id' => $this->user_id],
+            ['mobile' => $mobile, 'weixin_url' => $post['weixin_url'], 'update_time' => date('Y-m-d H:i:s')]);
+        //插入大神申请表
+        unset($post['token'],$post['mobile'],$post['code'],$post['weixin_url']);
+        $result_2 = $this->godApply->submitGodApply($post);
+        if ($result_1 && $result_2) {
+            $this->responseToJson(200, '提交成功,等待后续处理结果');
+        } else {
+            $this->responseToJson(502, '提交失败');
+        }
     }
 
     /**
@@ -62,17 +81,20 @@ class GodApply extends MY_Controller
     private function validPost($data)
     {
         $require = [
-            'game_type'=>'游戏类型',
-            'game_level_id'=>'段位id',
-            'level_url'=>'段位截图',
-            'can_zone'=>'可接大区',
-            'can_device'=>'可接设备系统',
-            'card_url'=>'身份证正面照'
+            'mobile' => '手机号',
+            'code' => '手机验证码',
+            'weixin_url' => '用户微信二维码',
+            'game_type' => '游戏类型', //游戏类型  1=>王者荣耀   2=>英雄联盟
+            'game_level_id' => '段位id',
+            'level_url' => '段位截图',
+            'can_zone' => '可接大区',//可接大区   1=>不限  2=>微信区   3=>QQ区
+            'can_device' => '可接设备系统', //可接设备系统  1=>不限  2=>IOS  3=>安卓
+            'card_url' => '身份证正面照'
         ];
         foreach ($require as $key => $val) {
             // 非空验证
             if (!isset($data[$key]) || empty($data[$key])) {
-                $this->responseJson(401, $val.' 不能为空');
+                $this->responseJson(401, $val . ' 不能为空');
                 break;
             }
         }
