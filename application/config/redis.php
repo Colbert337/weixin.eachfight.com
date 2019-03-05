2d1c41b4f15b8cecc2179088a48c1a8f0ac344b3
guochao <347653397@qq.com>
2019-03-05 15:53:21 +0800

    fix

--- application/config/redis.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') || exit('No direct script access allowed');
-
-$config['socket_type'] = 'tcp'; //`tcp` or `unix`
-// $config['socket'] = '/var/run/redis.sock'; // in case of `unix` socket type
-$config['host'] = '101.132.144.219';
-$config['password'] = 'aCBCYxk9YqpsS8redis';
-$config['port'] = 6379;
-$config['timeout'] = 0;
