a552034c76f0f6ef871a7bb9202c64d6ec6b338e
guochao <347653397@qq.com>
2019-03-05 16:32:35 +0800

    微信配置修改

--- application/config/wechat.php
+++ application/config/wechat.php
@@ -14,16 +14,14 @@ $config["wechat"] = [
      *
      * 当值为 false 时，所有的日志都不会记录
      */
-    'debug' => true,
+    'debug'   => true,
 
     /*
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
-//    'app_id' => 'wx9f3b71e6037e062e',         // AppID
-    'app_id' => 'wxabcdb4c431362dae',         // AppID
-//    'secret' => '32f98b33e33e395259e3aea0a0ffec70',     // AppSecret
-    'secret' => 'b59395ddf51ae3e07eb46c3b36b60aa3',     // AppSecret
-    'token' => 't9HazcCJVOcB4Rh5',          // Token
+    'app_id'  => 'wxabcdb4c431362dae',         // AppID
+    'secret'  => 'dfeeb5544b6603721442f889219eb746',     // AppSecret
+    'token'   => 't9HazcCJVOcB4Rh5',          // Token
     'aes_key' => 'pfVaL4o8Ra0Dy8orJAch3BGag0hpwkxBUK5HU2fwl9U',                    // EncodingAESKey
 
     /*
@@ -33,9 +31,9 @@ $config["wechat"] = [
      * debug/info/notice/warning/error/critical/alert/emergency
      * file：日志文件位置(绝对路径!!!)，要求可写权限
      */
-    'log' => [
+    'log'     => [
         'level' => 'debug',
-        'file' => APPPATH . 'logs/wechat.log',
+        'file'  => APPPATH . 'logs/wechat.log',
     ],
 
     /*
@@ -43,8 +41,8 @@ $config["wechat"] = [
      * scopes：公众平台（snsapi_userinfo / snsapi_base），开放平台：snsapi_login
      * callback：OAuth授权完成后的回调页地址(如果使用中间件，则随便填写。。。)
      */
-    'oauth' => [
-        'scopes' => ['snsapi_userinfo'],
+    'oauth'   => [
+        'scopes'   => ['snsapi_userinfo'],
         'callback' => 'api/weixin/oauthBack',
     ],
 
@@ -53,10 +51,10 @@ $config["wechat"] = [
      */
     'payment' => [
         'merchant_id' => '1490288602',
-        'key' => 'd86a3bc677466899c80b4ed1ce8be99a',
-        'cert_path' => APPPATH . 'key/wechat/apiclient_cert.pem',  //绝对路径
-        'key_path' => APPPATH . '/key/wechat/apiclient_key.pem',  //绝对路径
-        'notify_url' => ''
+        'key'         => 'd86a3bc677466899c80b4ed1ce8be99a',
+        'cert_path'   => APPPATH . 'key/wechat/apiclient_cert.pem',  //绝对路径
+        'key_path'    => APPPATH . '/key/wechat/apiclient_key.pem',  //绝对路径
+        'notify_url'  => ''
     ],
 
     'guzzle' => [
