40c1204e2b80651d7b773291a126fb88ec49aab9
guochao <347653397@qq.com>
2019-03-06 16:42:55 +0800

    微信配置fix

--- application/controllers/api/Weixin.php
+++ application/controllers/api/Weixin.php
@@ -56,7 +56,7 @@ class Weixin extends CI_Controller
             $jssdk->setUrl($url);
             $data = $jssdk->config(
                 ['onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone','chooseWXPay','chooseImage',
-                    'previewImage','uploadImage','downloadImage','getLocalImgData','getNetworkType'],
+                 'previewImage','uploadImage','downloadImage','getLocalImgData','getNetworkType'],
                 true, false, false);
             $this->responseToJson(200, '获取成功', $data);
         } catch (\Exception $exception) {
@@ -76,38 +76,9 @@ class Weixin extends CI_Controller
         if (empty($code)) $this->responseToJson(502, 'code参数缺少');
         log_message('info', 'weboauth获取到的code:' . $code);
 
-        $user = $this->wechat->oauth->user();
-        $data = $user->getOriginal();
-        var_dump($data);exit;
-        //随机token
-        $token = uuid();
-        //给token赋值并加密 设置有效期7200s
-        $this->cache->redis->save($token, md5($data['openid'] . 'eachfight'), 7200);
-        log_message('info', '授权获取用户随机token:' . $token);
-
-        //没有注册过 注册
-        if (!$this->User_Model->CheckRegister($data['openid'])) {
-            if (!$User_Model->insert([
-                'openid' => $data['openid'],
-                'token' => $token,
-                'nickname' => replace_emoji($data['nickname']),
-                'gender' => $data['sex'],  //1时是男性，值为2时是女性，值为0时是未知
-                'headimg_url' => $data['headimgurl'],
-                'create_time' => date('Y-m-d H:i:s')
-            ])) {
-                throw new \Exception('用户注册入库失败');
-            }
-        } else {  //更新token
-            if (!$User_Model->update(['openid' => $data['openid']],
-                ['token' => $token, 'update_time' => date('Y-m-d H:i:s')])) {
-                throw new \Exception('更新访问token失败');
-            }
-        }
-
         try {
             $user = $this->wechat->oauth->user();
             $data = $user->getOriginal();
-            var_dump($data);exit;
             //随机token
             $token = uuid();
             //给token赋值并加密 设置有效期7200s
